b7c125f2847d04d3c0a93e93bdb33a176c924021
GlebasusBP <bgp13art@Gmail.com>
2023-01-15 15:11:41 +0300

    feat: 20 passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let allTypesElements = [];
+  let pairOfElements = {};
+  let arr = [];
+
+  for(let i = 0; i < bracketsConfig.length; i++){
+    allTypesElements.push(bracketsConfig[i][0]);
+    pairOfElements[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  for(let i = 0; i < str.length; i++){
+    if(pairOfElements[str[i]] === arr[arr.length - 1] && arr.length !== 0){
+      arr.pop();
+    } else if(allTypesElements.includes(str[i])){
+      arr.push(str[i]);
+    } else {
+      if(arr.length === 0){
+        return false
+      }
+    }
+  }
+  
+  return arr.length === 0;
 }
